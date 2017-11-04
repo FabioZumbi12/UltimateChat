8d418b2c3f5d8101e545f3d116ad8970c50e069c
FabioZumbi12 <zumbi12doze@gmail.com>
2017-11-04 02:49:39 -0200

    [Spigot] better compat with tag plugins maded for LC.

--- UltimateChat-Spigot/src/main/java/br/com/devpaulo/legendchat/api/events/ChatMessageEvent.java
+++ UltimateChat-Spigot/src/main/java/br/com/devpaulo/legendchat/api/events/ChatMessageEvent.java
@@ -10,8 +10,6 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-import br.net.fabiozumbi12.UltimateChat.Bukkit.UChat;
-
 /**
  * Classe apenas para compatibilidade com o LegendChat para setar tags.
  * Só o método 'setTagValue' funciona nessa classe.
@@ -52,15 +50,12 @@ public class ChatMessageEvent extends Event implements Cancellable {
 		return true;
 	}
 	
-	public String getTagValue(String tag){	
-		tag = tag.toLowerCase();
+	public String getTagValue(String tag){
 		return tags.get(tag);
 	}
 	
 	public void addTag(String tagname, String value){
-		tagname = tagname.toLowerCase();
-		UChat.get().getAPI().registerNewTag(tagname, tagname, "", new ArrayList<String>(), "");
-		tags.put(tagname, (value==null?"":value));
+		tags.put(tagname, value);
 	}
 		
 	public HashMap<String,String> getTagMap(){
@@ -68,7 +63,7 @@ public class ChatMessageEvent extends Event implements Cancellable {
 	}
 	
 	public List<String> getTags(){
-		return UChat.get().getConfig().getTagList();
+		return new ArrayList<String>(tags.keySet());
 	}
 	
 	@Override
