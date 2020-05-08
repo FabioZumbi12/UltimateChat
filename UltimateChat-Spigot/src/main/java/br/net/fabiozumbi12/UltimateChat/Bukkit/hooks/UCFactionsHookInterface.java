4706e118caf8780139348895d9cf2bcc66cba08c
Sean McClenaghan <cyberkitsune09@gmail.com>
2020-05-07 19:18:28 -0700

    Finish saberfactions hook

--- UltimateChat-Spigot/src/main/java/br/net/fabiozumbi12/UltimateChat/Bukkit/hooks/UCFactionsHookInterface.java
+++ UltimateChat-Spigot/src/main/java/br/net/fabiozumbi12/UltimateChat/Bukkit/hooks/UCFactionsHookInterface.java
@@ -3,5 +3,5 @@ package br.net.fabiozumbi12.UltimateChat.Bukkit.hooks;
 import org.bukkit.entity.Player;
 
 public interface UCFactionsHookInterface {
-    public String formatFac(String text, Player sender, Object receiver);
+    String formatFac(String text, Player sender, Object receiver);
 }
--- UltimateChat-Spigot/src/main/java/br/net/fabiozumbi12/UltimateChat/Bukkit/hooks/UCSFactionsHook.java
+++ UltimateChat-Spigot/src/main/java/br/net/fabiozumbi12/UltimateChat/Bukkit/hooks/UCSFactionsHook.java
@@ -1,11 +1,28 @@
 package br.net.fabiozumbi12.UltimateChat.Bukkit.hooks;
 
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
 import org.bukkit.entity.Player;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class UCSFactionsHook implements UCFactionsHookInterface {
     @Override
     public String formatFac(String text, Player sender, Object receiver) {
-        throw new NotImplementedException();
+        FPlayer fp = FPlayers.getInstance().getByPlayer(sender);
+        if (!fp.getFaction().isWilderness()) {
+            Faction fac = fp.getFaction();
+            text = text
+                    .replace("{fac-id}", fac.getId())
+                    .replace("{fac-name}", fac.getTag())
+                    .replace("{fac-description}", fac.getDescription());
+           // Saber doesn't have MOTD support
+            if (receiver instanceof Player) {
+                FPlayer recmp = FPlayers.getInstance().getByPlayer((Player) receiver);
+                text = text
+                        .replace("{fac-relation-name}", fac.getTag(recmp))
+                        .replace("{fac-relation-color}", fac.getColorTo(recmp).toString());
+            }
+        }
+        return text;
     }
 }
